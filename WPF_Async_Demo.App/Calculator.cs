c51781fb247bf2e32b997574d0d7a7566f304aaa
Starinin Andrey <starininandrey@gmail.com>
2022-11-11 19:44:53 +0300

    Init

---
+++ WPF_Async_Demo.App/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace WPF_Async_Demo.App;
+
+public static class Calculator
+{
+    public static long Factorial(int number)
+    {
+        long factorial = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+            Thread.Sleep(1000);
+        }
+
+        return factorial;
+    }
+
+    public static async Task<long> FactorialAsync(int number)
+    {
+        return await Task.Run(() => Factorial(number));
+    }
+}
---
+++ WPF_Async_Demo.App/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_Async_Demo.App
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private async void ButtonStartTask1_OnClick(object sender, RoutedEventArgs e)
+        {
+            Task1Output.Text = (await Calculator.FactorialAsync(10)).ToString();
+        }
+        
+        private async void ButtonStartTask2_OnClick(object sender, RoutedEventArgs e)
+        {
+            await TaskSleep(Task2Output);
+        }
+
+        private async Task TaskSleep(TextBlock output)
+        {
+            output.Text = "Start";
+            await Task.Delay(3000);
+            output.Text = "Completed";
+        }
+    }
+}
